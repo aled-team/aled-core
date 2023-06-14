4241b20e0a36b0f459e9c8ba94216175db78e8be
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-06-14 15:10:24 +0200

    Initial Commit

---
+++ aled-core/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aled-core
+//
+//  Created by Nicolas de Garrigues on 14/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aled-core/aled_coreApp.swift
@@ -0,0 +1,17 @@
+//
+//  aled_coreApp.swift
+//  aled-core
+//
+//  Created by Nicolas de Garrigues on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct aled_coreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
